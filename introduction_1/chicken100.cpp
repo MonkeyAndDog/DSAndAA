739d3898dff8df9abd0fef68d7a080633749ed58
Monkey <1466947023@qq.com>
2018-11-02 20:39:33 +0800

    增加忽略文档

--- introduction_1/chicken100.cpp
+++ introduction_1/chicken100.cpp
@@ -1,13 +1,14 @@
-//
-// Created by Mr.Hu on 2018/11/2.
-//
+/**
+ * Created by Xiaozhong on 2018/11/2.
+ * Copyright (c) 2018/11/2 Xiaozhong. All rights reserved.
+ */
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-
-    int count = 0;
+    int count = 0;  //存下来符合要求的方案数量
     for (int i = 0; i <= 33; ++i) {  //母鸡购买个数
         for (int j = 0; j <= 100; ++j) { //公鸡购买个数
             for (int k = 0; k <= 100; ++k) { //小鸡购买个数
@@ -21,4 +22,3 @@ int main() {
 
     cout << "共计" << count << "种解决方案" << endl;
 }
-
